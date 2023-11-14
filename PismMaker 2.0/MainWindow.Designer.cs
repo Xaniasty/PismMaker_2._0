77140086cc86e2968a17e6f0392acc13906b30e5
Xaniasty <xpoloxinix@gmail.com>
2023-11-14 01:35:10 +0100

    Wprowadzono zmiany kolorystyczne (malowanie gradientu). Wprowadzono Labele z opisami pól.

--- PismMaker 2.0/MainWindow.Designer.cs
+++ PismMaker 2.0/MainWindow.Designer.cs
@@ -28,22 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelAppName = new Label();
+            labelChooseTeam = new Label();
+            labelChooseTemplate = new Label();
+            labelOptions = new Label();
+            labelClientPanel = new Label();
+            labelQuestions = new Label();
+            labelControlPanel = new Label();
+            labelAttachments = new Label();
             SuspendLayout();
+            // 
+            // labelAppName
+            // 
+            labelAppName.BackColor = SystemColors.Menu;
+            labelAppName.BorderStyle = BorderStyle.Fixed3D;
+            labelAppName.FlatStyle = FlatStyle.Popup;
+            labelAppName.Font = new Font("Rockwell Extra Bold", 21.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            labelAppName.ForeColor = Color.OrangeRed;
+            labelAppName.Location = new Point(864, 10);
+            labelAppName.Name = "labelAppName";
+            labelAppName.Size = new Size(223, 85);
+            labelAppName.TabIndex = 0;
+            labelAppName.Text = "PismMaker 2.0";
+            labelAppName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelChooseTeam
+            // 
+            labelChooseTeam.BackColor = SystemColors.AppWorkspace;
+            labelChooseTeam.BorderStyle = BorderStyle.Fixed3D;
+            labelChooseTeam.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelChooseTeam.Location = new Point(310, 9);
+            labelChooseTeam.Name = "labelChooseTeam";
+            labelChooseTeam.Size = new Size(548, 38);
+            labelChooseTeam.TabIndex = 1;
+            labelChooseTeam.Text = "Wybierz zespół:";
+            // 
+            // labelChooseTemplate
+            // 
+            labelChooseTemplate.BackColor = SystemColors.ActiveBorder;
+            labelChooseTemplate.BorderStyle = BorderStyle.Fixed3D;
+            labelChooseTemplate.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelChooseTemplate.Location = new Point(310, 57);
+            labelChooseTemplate.Name = "labelChooseTemplate";
+            labelChooseTemplate.Size = new Size(548, 38);
+            labelChooseTemplate.TabIndex = 2;
+            labelChooseTemplate.Text = "Wybór Szablonu:";
+            // 
+            // labelOptions
+            // 
+            labelOptions.BackColor = SystemColors.AppWorkspace;
+            labelOptions.BorderStyle = BorderStyle.Fixed3D;
+            labelOptions.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelOptions.Location = new Point(12, 10);
+            labelOptions.Name = "labelOptions";
+            labelOptions.Size = new Size(280, 288);
+            labelOptions.TabIndex = 3;
+            labelOptions.Text = "Opcje:";
+            // 
+            // labelClientPanel
+            // 
+            labelClientPanel.BackColor = SystemColors.ActiveBorder;
+            labelClientPanel.BorderStyle = BorderStyle.Fixed3D;
+            labelClientPanel.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelClientPanel.Location = new Point(310, 111);
+            labelClientPanel.Name = "labelClientPanel";
+            labelClientPanel.Size = new Size(777, 187);
+            labelClientPanel.TabIndex = 4;
+            labelClientPanel.Text = "Dane do pisma";
+            // 
+            // labelQuestions
+            // 
+            labelQuestions.BackColor = SystemColors.ActiveBorder;
+            labelQuestions.BorderStyle = BorderStyle.Fixed3D;
+            labelQuestions.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelQuestions.Location = new Point(310, 312);
+            labelQuestions.Name = "labelQuestions";
+            labelQuestions.Size = new Size(777, 272);
+            labelQuestions.TabIndex = 5;
+            labelQuestions.Text = "Wybierz pytania";
+            // 
+            // labelControlPanel
+            // 
+            labelControlPanel.BackColor = SystemColors.AppWorkspace;
+            labelControlPanel.BorderStyle = BorderStyle.Fixed3D;
+            labelControlPanel.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelControlPanel.Location = new Point(12, 312);
+            labelControlPanel.Name = "labelControlPanel";
+            labelControlPanel.Size = new Size(280, 272);
+            labelControlPanel.TabIndex = 6;
+            labelControlPanel.Text = "Panel Sterowania";
+            // 
+            // labelAttachments
+            // 
+            labelAttachments.BackColor = SystemColors.ActiveBorder;
+            labelAttachments.BorderStyle = BorderStyle.Fixed3D;
+            labelAttachments.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            labelAttachments.Location = new Point(310, 603);
+            labelAttachments.Name = "labelAttachments";
+            labelAttachments.Size = new Size(777, 214);
+            labelAttachments.TabIndex = 7;
+            labelAttachments.Text = "Załączniki";
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
-            this.MaximizeBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            ClientSize = new Size(1099, 782);
+            ClientSize = new Size(1099, 826);
+            Controls.Add(labelAttachments);
+            Controls.Add(labelControlPanel);
+            Controls.Add(labelQuestions);
+            Controls.Add(labelClientPanel);
+            Controls.Add(labelOptions);
+            Controls.Add(labelChooseTemplate);
+            Controls.Add(labelChooseTeam);
+            Controls.Add(labelAppName);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "MainWindow";
-            Text = "MainWindow";
-            Load += MainWindow_Load_1;
+            Text = "Okno Główne";
+            Load += MainWindow_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label labelAppName;
+        private Label labelChooseTeam;
+        private Label labelChooseTemplate;
+        private Label labelOptions;
+        private Label labelClientPanel;
+        private Label labelQuestions;
+        private Label labelControlPanel;
+        private Label labelAttachments;
     }
 }
--- PismMaker 2.0/MainWindow.cs
+++ PismMaker 2.0/MainWindow.cs
@@ -7,7 +7,9 @@ using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.Reflection;
 using System.IO;
-
+using System;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace PismMaker_2._0
 {
@@ -16,6 +18,7 @@ namespace PismMaker_2._0
         public MainWindow()
         {
             InitializeComponent();
+            this.Paint += MainForm_Paint;
         }
 
         private void MainWindow_Load(object sender, EventArgs e)
@@ -23,9 +26,24 @@ namespace PismMaker_2._0
 
         }
 
-        private void MainWindow_Load_1(object sender, EventArgs e)
+        private void MainForm_Paint(object sender, PaintEventArgs e)
         {
+            Color darkBlue = Color.FromArgb(0, 0, 128); // Ciemny niebieski
+            Color lightBlue = Color.FromArgb(135, 206, 235); // Jasny niebieski
+            Color orange = Color.FromArgb(255, 165, 0); //pomarañczowy
+            Color deepBlue = Color.FromArgb(70, 130, 180); //b³êkitny
+            Color black = Color.FromArgb(0, 0, 0); //czarny
 
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                darkBlue,
+                black,
+                LinearGradientMode.Vertical))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
         }
+
     }
 }
